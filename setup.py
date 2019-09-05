9d147f55dae5e7612d33d4a18eed7411bc9da42d
AndrewMarksArt <andrew.marks@cgu.edu>
2019-09-04 20:44:27 -0700

    moved __init__.py and updated version to 0.0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata-AndrewMarksArt",
-    version="0.1.0",
+    version="0.0.6",
     author="AndrewMarksArt",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
